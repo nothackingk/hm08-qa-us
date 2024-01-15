7e644e4f2cd8f9c2d3465f708d413de238107026
ray grigolla <raygrigolla@gmail.com>
2024-01-15 11:00:45 -0800

    Final

--- test/specs/createAnOrder.e2e.js
+++ test/specs/createAnOrder.e2e.js
@@ -12,51 +12,68 @@ const helper = require('../../helper')
         await expect(pnoneNumberModal).toBeExisting();
     }) */
 
-    it('should wait for the taxi driver', async () => {
+    it('should enter address', async () => {
         //call the taxi to the address
         await browser.url(`/`)
         await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
+    })
 
+    it('should select supportive mode ', async () => {
         //select supportive mode 
         const supportiveCarType = await $(page.supportiveCarType);
         await supportiveCarType.waitForDisplayed();
         await supportiveCarType.click();
         await expect(supportiveCarType).toBeClickable();
+    })
 
+    it('should input phone number ', async () => {
         //input phone number
         const phoneNumber = helper.getPhoneNumber("+1");
         await page.submitPhoneNumber(phoneNumber);
         await expect(await helper.getElementByText(phoneNumber)).toBeExisting();
-        
+    })
+
+    it('should add payment method ', async () => {    
         //adding a payment card
         await page.addPaymentMethodCard();
         
         const cardPaymentMethodIcon = await $(page.cardPaymentMethodIcon);
         await cardPaymentMethodIcon.waitForDisplayed();
         await expect(await $(cardPaymentMethodIcon)).toBeExisting();
+    })
 
+    it('should add message to driver ', async () => {
         //message to driver
         const messageInput = await $(page.messageInput);
         await messageInput.waitForDisplayed();
-        await messageInput.click();
         await messageInput.setValue("Hello");
+    })
 
+    it('should select Blanket and Handkerchiefs ', async () => {
         //Ordering a Blanket and handkerchiefs
         const BlanketHandkerchiefsButton = await $(page.BlanketHandkerchiefsButton);
         await BlanketHandkerchiefsButton.waitForDisplayed();
         await BlanketHandkerchiefsButton.click();
         await expect(BlanketHandkerchiefsButton).toBeEnabled();
+    })
 
+    it('should order 2 ice creams ', async () => {
         //Order 2 ice creams
         await page.addIceCreamOrder();
+    })
 
+    it('should order the car ', async () => {
         //submit car order
         const submitCarOrder = await $(page.submitCarOrder);
         await submitCarOrder.waitForDisplayed();
         await submitCarOrder.click();
-        await expect(submitCarOrder).toBeClickable();
-        await browser.pause(20000);
-
     })
+
+    it('browser pause for car order details ', async () => {
+        //browser pause
+        await browser.pause(30000);
+    })
+
+    
 
 
--- wdio.conf.js
+++ wdio.conf.js
@@ -20,7 +20,7 @@ exports.config = {
 ],
     logLevel: 'error',
     bail: 0,
-    baseUrl: 'https://255eab76-5104-4ed5-9389-c4533a8eadbd.serverhub.tripleten-services.com',
+    baseUrl: 'https://918da76a-1831-4dc9-91bf-ef33ff56e3ee.serverhub.tripleten-services.com',
     waitforTimeout: 10000,
     connectionRetryTimeout: 120000,
     connectionRetryCount: 3,
